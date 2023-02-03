d9ca0c5b0c5931d8dbe767eb1d2113cfb3d4dd5f
JossueAlejandroCandelasHernandez <al222110811@gmail.com>
2023-02-02 20:39:31 -0600

    First commit for practices

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Jossue Alejandro Candelas Hernández
         </p>
         <a
           className="App-link"
