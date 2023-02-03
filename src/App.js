f40d9c90663bde61622a5ee087f2cbb45f6ee8f7
JossueAlejandroCandelasHernandez <al222110811@gmail.com>
2023-02-02 20:43:56 -0600

    Cambio en la rama developer Archivo App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Jossue Alejandro Candelas Hernández
+          App <code></code> app.js
         </p>
         <a
           className="App-link"
